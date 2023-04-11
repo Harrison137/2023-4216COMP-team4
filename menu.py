7a393c8e362be446a66c5bd88b6ef3c0238b9ce7
Adam <adamheyes2004@gmail.com>
2023-04-11 16:22:20 +0100

    .

--- menu.py
+++ menu.py
@@ -296,7 +296,7 @@ def options():
     print("[13] Option 13: Profit margin per sub category")
     print("[14] Option 14: Show top 10 states by quantity of items bought")
     print("[15] Option 15: Most profitable item in a state")
-    print("[14] Option 14: Print average waiting time per product")
+    print("[16] Option 16: Print average waiting time per product")
 
 while True:
     options()
