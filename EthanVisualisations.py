eb4e79e7c275c8034ae2c78f328facbcdda392c8
EthanWelsh04 <CSMEWELS@ljmu.ac.uk>
2023-04-24 18:28:50 +0100

    Added 2 more

--- EthanVisualisations.py
+++ EthanVisualisations.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 df = pd.read_csv('Sample - Superstore.csv', encoding='windows-1252')
 
 def option1():
@@ -54,13 +54,39 @@ def option4():
     plt.xticks(rotation=45)
     # Show the plot
     plt.show()
-#prints oprions to the user
+def option5():
+    #this makes sure that only categorys that are technolgy are counted
+    onlyTechnologyCata = df[df['Category'] == 'Technology']
+    #groups the tech catagory by state and calculates the sum of the quantity for each group
+    groupedWithAll = onlyTechnologyCata.groupby(['State', 'Category']).agg({'Quantity': 'sum'}).reset_index()
+    #sort the data by decending order by quantity (the top 10 states)
+    groupedAndSorted = groupedWithAll.sort_values('Quantity', ascending=False).head(10)
+    # Title + Print the best selling category (by quantity) for each state
+    plt.bar(groupedAndSorted['State'], groupedAndSorted['Quantity'])
+    plt.xticks(rotation=45)
+    plt.title('Top 10 states that sold the most technology')
+    plt.xlabel('State')
+    plt.ylabel('Quantity')
+    plt.show()
+def option6():
+    #groups by region and sums the discounts
+    discount_by_highest = df.groupby('State').sum({'Discount': 'sum'}).sort_values('Discount', ascending=False).head(10)
+    #gives a title
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(data=discount_by_highest[['Discount']], annot=True, fmt=".2f", cmap="YlGnBu", cbar_kws={'label': 'Discount Amount'})
+    plt.title("Amount Given In Discounts By Region (Highest to Lowest)")
+    plt.xlabel("State")
+    plt.ylabel("Discount")
+    plt.show()
+
 def options():
     print("[0] To leave")
     print("[1] Option 1: Quantity by Month")
     print("[2] Option 2: Regional Discounts")
     print("[3] Option 3: Catagory Profit Percentage")
     print("[4] Option 4: Discounts Per Year")
+    print("[5] Option 5: Top 10 states that sold the most technolog")
+    print("[6] Option 6: Amount Given In Discounts By Region (Highest to Lowest)")
 #Running the code
 while True:
     options()
@@ -75,3 +101,9 @@ while True:
         option3()
     elif option==4:
         option4()
+    elif option==5:
+        option5()
+    elif option==6:
+        option6()
+
+
